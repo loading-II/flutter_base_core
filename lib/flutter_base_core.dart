85ba95c2637538618e07b02fb481fb2ca64c8d6e
β <loading_vip@126.com>
2024-03-01 17:23:40 +0800

    page_route_annimation

--- lib/flutter_base_core.dart
+++ lib/flutter_base_core.dart
@@ -4,3 +4,5 @@ export 'src/http_net/proxy/http_proxy.dart';
 export 'src/http_net/http_helper.dart';
 export 'src/http_net/entity/http_entity.dart';
 export 'src/http_net/handler_response/i_handler_response.dart';
+
+export 'src/extends/page_route_annimation.dart';
